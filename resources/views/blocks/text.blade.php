547c43a053e0037a0d9796148afdc13837b20b48
unknown <mr.shahbaznns@gmail.com>
2026-09-01 18:38:58 +0500

    feat(ui): make glossary links distinct with interactive accent styling and high contrast on colored blocks

--- resources/views/blocks/text.blade.php
+++ resources/views/blocks/text.blade.php
@@ -1,7 +1,7 @@
 @php($s = \App\Support\LandingBlock::settings($block))
 <section class="{{ $s['padding_class'] }} {{ $s['animation_class'] }}" {!! $s['inline_style'] ? 'style="'.$s['inline_style'].'"' : '' !!}>
     <div class="{{ $s['container_class'] ?: 'mx-auto max-w-4xl px-6 lg:px-8' }} {{ $s['text_align_class'] }}">
-        <div class="prose max-w-none [color:inherit] [&_h1]:[color:inherit] [&_h2]:[color:inherit] [&_h3]:[color:inherit] [&_h4]:[color:inherit] [&_p]:[color:inherit] [&_strong]:[color:inherit] [&_span]:[color:inherit] [&_a]:[color:inherit] [&_a]:underline [&_a]:decoration-current/40 hover:[&_a]:decoration-current {{ $s['rounded_class'] ? $s['rounded_class'].' p-6 ' : '' }}{{ $s['border_class'] ? $s['border_class'].' ' : '' }}">
+        <div class="prose max-w-none [color:inherit] [&_h1]:[color:inherit] [&_h2]:[color:inherit] [&_h3]:[color:inherit] [&_h4]:[color:inherit] [&_p]:[color:inherit] [&_strong]:[color:inherit] {{ $s['rounded_class'] ? $s['rounded_class'].' p-6 ' : '' }}{{ $s['border_class'] ? $s['border_class'].' ' : '' }}">
             {!! $block['content'] ?? '' !!}
         </div>
     </div>
